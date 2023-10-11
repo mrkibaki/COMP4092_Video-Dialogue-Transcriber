85c97e14b5f3e5a80d71bd01a47852c50cb3ee76
mrkibaki <chenxu.lin@students.mq.edu.au>
2023-10-11 23:35:27 +1100

    Adaptive eye detection.

--- features/eye_detection.py
+++ features/eye_detection.py
@@ -19,14 +19,20 @@ def detect_pupils(eye_region, frame, start_point):
     # 转换为灰度图像
     gray_eye = cv2.cvtColor(eye_region, cv2.COLOR_BGR2GRAY)
 
-    # 使用Otsu's方法进行二值化
-    _, thresh = cv2.threshold(gray_eye, 70, 255, cv2.THRESH_BINARY)
+    # 使用Otsu's方法进行二值化（threshholding）
+    block_size = 11  # 通常为奇数，表示计算阈值时使用的区域大小
+    C = 2  # 一个常数，从计算的平均值或加权平均值中减去的常数
+    thresh = cv2.adaptiveThreshold(gray_eye, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, block_size, C)
+
+    # 1. 使用膨胀操作处理阈值化后的图像
+    kernel = np.ones((3, 3), np.uint8)
+    dilated_eye = cv2.dilate(thresh, kernel, iterations=2)
 
     # 在Otsu's阈值化后，显示二值化的眼睛图像
     # cv2.imshow("Thresholded Eye", thresh)
 
     # 查找轮廓
-    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(dilated_eye, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if contours:  # 检查是否有轮廓被检测到
         c = max(contours, key=cv2.contourArea)  # 获取最大的轮廓
         M = cv2.moments(c)
