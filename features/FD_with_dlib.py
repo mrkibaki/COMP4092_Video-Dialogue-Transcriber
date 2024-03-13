79febdf99ceb99e920e6f11ed26d0273a19e9edc
mrkibaki <chenxu.lin@students.mq.edu.au>
2024-03-13 20:06:42 +0800

    Videoisation of all operations of the MTCNN

--- features/FD_with_dlib.py
+++ features/FD_with_dlib.py
@@ -5,18 +5,26 @@ import numpy as np
 from features.GazeTracking.gaze_tracking.gaze_tracking import GazeTracking
 
 current_path = os.path.dirname(os.path.abspath(__file__))
+video_path = os.path.join(current_path, "tester.mp4")
 model_path = os.path.join(current_path, "shape_predictor_68_face_landmarks.dat")
 
-# detector = dlib.get_frontal_face_detector()  # 使用HOG检测器
-# MTCNN
 cnn_model_path = os.path.join(current_path, "mmod_human_face_detector.dat")
 cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_model_path)
 
 predictor = dlib.shape_predictor(model_path)
-
 gaze = GazeTracking()
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(video_path)
+
+# 获取视频的宽度、高度和帧率
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+fps = cap.get(cv2.CAP_PROP_FPS)
+
+# 定义VideoWriter对象
+output_path = os.path.join(current_path, "output_video.avi")
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter(output_path, fourcc, fps, (frame_width, frame_height))
 
 while True:
     ret, frame = cap.read()
@@ -26,7 +34,6 @@ while True:
     # 可以选择进一步缩小图像尺寸来提高速度
     small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
     gray = cv2.cvtColor(small_frame, cv2.COLOR_BGR2GRAY)
-    # faces = detector(gray, 1)  # 使用HOG检测器
     faces = cnn_face_detector(gray, 1)  # 使用MTCNN
 
     for face in faces:
@@ -90,49 +97,8 @@ while True:
     cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
     cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
-    ##############################################################
-    # # Eye pupil
-    # for eye_points in [range(36, 42), range(42, 48)]:
-    #     eye = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in eye_points])
-    #     x, y, w, h = cv2.boundingRect(eye)
-    #     eye_image = gray[y:y + h, x:x + w]
-    #
-    #     # 应用高斯模糊
-    #     eye_image = cv2.GaussianBlur(eye_image, (7, 7), 0)
-    #
-    #     # 使用高斯模糊减少噪声
-    #     eye_image_blurred = cv2.GaussianBlur(eye_image, (7, 7), 0)
-    #
-    #     # 检测圆 - 虹膜
-    #     circles = cv2.HoughCircles(eye_image_blurred, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30,
-    #                                minRadius=int(h / 4), maxRadius=int(h / 2))
-    #
-    #     if circles is not None:
-    #         circles = np.round(circles[0, :]).astype("int")
-    #         for (cx, cy, r) in circles:
-    #             # 绘制虹膜边缘
-    #             cv2.circle(frame, ((cx + x) * 2, (cy + y) * 2), r * 2, (255, 255, 0), 4)
-    #             # 绘制虹膜中心
-    #             cv2.circle(frame, ((cx + x) * 2, (cy + y) * 2), 2, (0, 255, 0), -1)
-    #
-    #     # 使用自适应阈值
-    #     _, thresholded_eye = cv2.threshold(eye_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-    #
-    #     # 形态学操作
-    #     kernel = np.ones((2, 2), np.uint8)
-    #     thresholded_eye = cv2.erode(thresholded_eye, kernel, iterations=2)
-    #     thresholded_eye = cv2.dilate(thresholded_eye, kernel, iterations=4)
-    #     thresholded_eye = cv2.erode(thresholded_eye, kernel, iterations=2)
-    #
-    #     contours, _ = cv2.findContours(thresholded_eye, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    #     if contours:
-    #         largest_blob = max(contours, key=cv2.contourArea)
-    #         M = cv2.moments(largest_blob)
-    #         if M['m00'] != 0:
-    #             cx = int(M['m10'] / M['m00'])
-    #             cy = int(M['m01'] / M['m00'])
-    #             pupil_center_global = ((cx + x) * 2, (cy + y) * 2)
-    #             cv2.circle(frame, pupil_center_global, 3, (0, 0, 255), -1)
+    # 保存帧到输出视频
+    out.write(frame)
 
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1)
@@ -140,4 +106,49 @@ while True:
         break
 
 cap.release()
+out.release()  # 释放VideoWriter对象
 cv2.destroyAllWindows()
+
+##############################################################
+# # Eye pupil
+# for eye_points in [range(36, 42), range(42, 48)]:
+#     eye = np.array([(landmarks.part(n).x, landmarks.part(n).y) for n in eye_points])
+#     x, y, w, h = cv2.boundingRect(eye)
+#     eye_image = gray[y:y + h, x:x + w]
+#
+#     # 应用高斯模糊
+#     eye_image = cv2.GaussianBlur(eye_image, (7, 7), 0)
+#
+#     # 使用高斯模糊减少噪声
+#     eye_image_blurred = cv2.GaussianBlur(eye_image, (7, 7), 0)
+#
+#     # 检测圆 - 虹膜
+#     circles = cv2.HoughCircles(eye_image_blurred, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30,
+#                                minRadius=int(h / 4), maxRadius=int(h / 2))
+#
+#     if circles is not None:
+#         circles = np.round(circles[0, :]).astype("int")
+#         for (cx, cy, r) in circles:
+#             # 绘制虹膜边缘
+#             cv2.circle(frame, ((cx + x) * 2, (cy + y) * 2), r * 2, (255, 255, 0), 4)
+#             # 绘制虹膜中心
+#             cv2.circle(frame, ((cx + x) * 2, (cy + y) * 2), 2, (0, 255, 0), -1)
+#
+#     # 使用自适应阈值
+#     _, thresholded_eye = cv2.threshold(eye_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+#
+#     # 形态学操作
+#     kernel = np.ones((2, 2), np.uint8)
+#     thresholded_eye = cv2.erode(thresholded_eye, kernel, iterations=2)
+#     thresholded_eye = cv2.dilate(thresholded_eye, kernel, iterations=4)
+#     thresholded_eye = cv2.erode(thresholded_eye, kernel, iterations=2)
+#
+#     contours, _ = cv2.findContours(thresholded_eye, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+#     if contours:
+#         largest_blob = max(contours, key=cv2.contourArea)
+#         M = cv2.moments(largest_blob)
+#         if M['m00'] != 0:
+#             cx = int(M['m10'] / M['m00'])
+#             cy = int(M['m01'] / M['m00'])
+#             pupil_center_global = ((cx + x) * 2, (cy + y) * 2)
+#             cv2.circle(frame, pupil_center_global, 3, (0, 0, 255), -1)
