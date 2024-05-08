14da10b866d76887b39f4ff96ec758219219bfbf
mrkibaki <chenxu.lin@students.mq.edu.au>
2024-05-08 21:33:45 +0800

    fix threshold bug

--- features/FaceModalAnalysisAlgorithm/BrowModalities.py
+++ features/FaceModalAnalysisAlgorithm/BrowModalities.py
@@ -71,15 +71,10 @@ def RaiseEyebrow(landmarks, neutral_data):
 
     eye_width = OuterEyePointDistance(landmarks)
     norm_brow_dist = brow_dist / eye_width
-    threshold = neutral_data['ebdist'] * 1.1
+    threshold = neutral_data['ebdist'] * 1.15
 
     if norm_brow_dist >= threshold:
-        # 如果大于或等于阈值，可能性为0（未皱眉）
-        return 0
-    else:
-        # 如果小于阈值，根据距离与阈值的比例返回一个可能性评分
-        # 比例越小，可能性评分越高，这里使用 1 - (比例) 来表示
-        # 用指数函数来增加数值的敏感度。
-        possibility = (min(1, max(0, np.exp(7 * (1 - norm_brow_dist / threshold)) - 1))) * 100
-
+        possibility = (min(1, max(0, np.exp(3 * (norm_brow_dist - threshold) / threshold)))) * 100
         return possibility
+    else:
+        return 0
