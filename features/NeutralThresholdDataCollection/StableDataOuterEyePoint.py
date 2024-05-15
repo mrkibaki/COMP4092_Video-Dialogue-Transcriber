6f17a5bed169c2f476c247ce5a9a48338ed41deb
mrkibaki <chenxu.lin@students.mq.edu.au>
2024-05-16 02:06:39 +1000

    landmarks selection bug fixed

--- features/NeutralThresholdDataCollection/StableDataOuterEyePoint.py
+++ features/NeutralThresholdDataCollection/StableDataOuterEyePoint.py
@@ -1,10 +1,12 @@
 import numpy as np
+import csv
+import os
 
 
 def OuterEyePointDistance(landmarks):
     # Calculate the Euclidean distance between the outer corners of the eyes
     right_eye_outer = np.array([landmarks.part(45).x, landmarks.part(45).y])
-    left_eye_outer = np.array([landmarks.part(42).x, landmarks.part(42).y])
+    left_eye_outer = np.array([landmarks.part(36).x, landmarks.part(36).y])
     eye_width = np.linalg.norm(right_eye_outer - left_eye_outer)
 
     return eye_width
